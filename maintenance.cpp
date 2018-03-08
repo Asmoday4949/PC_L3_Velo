8e18db5a8cfb2d3d3ce32a2f6b185fdf7820cbf0
Malik Fleury <malik.fleury@bluewin.ch>
2018-03-08 19:05:28 +0100

    Cleaned useless comments

--- maintenance.cpp
+++ maintenance.cpp
@@ -13,51 +13,6 @@ Maintenance::~Maintenance()
 
 void Maintenance::run()
 {
- /*
-1. Mettre a = min(2;D) vélos dans la camionnette
-2. Pour i = 1 à S faire
-    2a. Si Vi > B - 2 alors
-        Prendre c = min(Vi - (B - 2); 4 - a) et les mettre dans la camionnette.
-        a <- a + c
-    2b. Si Vi < B - 2 alors
-        Laisser c = min((B - 2) - Vi; a)
-        a <- a - c
-3. Vider la camionnette D <- D + a.
-4. Faire une pause.
-*/
-
-    /*
-    //depot = -1 if moving from depot, nbSite if moving to depot
-    AlgoThread* algo = AlgoThread::getAlgoThread();
-
-    this->position = this->idDepot;
-
-    while(true)
-    {
-        //depot is not protected by a mutex because there is only one maintenance
-        //and that's the only thread who take and put in the depot
-        this->takeFromDepot();
-
-        for(int i = 0; i < algo->getNbSite(); i++)
-        {
-            int tripTime = AlgoThread::getRandomTripTime();
-            emit algo->startCamionDeplacement(this->position, i, tripTime);
-            this->position = i;
-            this->sleep(tripTime);
-
-            emit algo->setSiteVelo(i, 100);
-        }
-
-        int tripTime = AlgoThread::getRandomTripTime();
-        emit algo->startCamionDeplacement(this->position, algo->getNbSite(), tripTime);
-        this->position = this->idDepot;
-        this->sleep(tripTime);
-
-        this->dropAtDepot();
-        this->sleep(this->breakTime);
-    }
-    */
-
     work();
 }
 
